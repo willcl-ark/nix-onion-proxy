127e2d5bfdad8d15e59dd1c7882d28d69ccc28b0
willcl-ark <will@256k1.dev>
2024-11-21 11:38:24 +0000

    dns over TLS

--- configuration.nix
+++ configuration.nix
@@ -30,11 +30,11 @@
     users = {
       root = {
         hashedPassword = "*"; # Disable root password
-        openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH988C5DbEPHfoCphoW23MWq9M6fmA4UTXREiZU0J7n0 will.hetzner@temp.com" ]
+        openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH988C5DbEPHfoCphoW23MWq9M6fmA4UTXREiZU0J7n0 will.hetzner@temp.com" ];
       };
       nixos = {
         hashedPassword = "$y$j9T$qXvXoVtyyX0mWlq5n7ftw/$R7HLZKI6koJIMHTtvGDJOqoAhGO40/OtDLkWZhPEJPC";
-        openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH988C5DbEPHfoCphoW23MWq9M6fmA4UTXREiZU0J7n0 will.hetzner@temp.com" ]
+        openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH988C5DbEPHfoCphoW23MWq9M6fmA4UTXREiZU0J7n0 will.hetzner@temp.com" ];
         isNormalUser = true;
         extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
       };
@@ -60,6 +60,64 @@
         members = [ "caddyTor" ];
       };
     };
+  };
+
+  ## DNS-over-TLS
+  services.stubby = {
+    enable = true;
+    settings = {
+      # ::1 cause error, use 0::1 instead
+      listen_addresses = [ "127.0.0.1" "0::1" ];
+      # https://github.com/getdnsapi/stubby/blob/develop/stubby.yml.example
+      resolution_type = "GETDNS_RESOLUTION_STUB";
+      dns_transport_list = [ "GETDNS_TRANSPORT_TLS" ];
+      tls_authentication = "GETDNS_AUTHENTICATION_REQUIRED";
+      tls_query_padding_blocksize = 128;
+      idle_timeout = 10000;
+      round_robin_upstreams = 1;
+      tls_min_version = "GETDNS_TLS1_3";
+      dnssec = "GETDNS_EXTENSION_TRUE";
+      upstream_recursive_servers = [
+        {
+          address_data = "1.1.1.2";
+          tls_auth_name = "cloudflare-dns.com";
+        }
+        {
+          address_data = "1.0.0.2";
+          tls_auth_name = "cloudflare-dns.com";
+        }
+        {
+          address_data = "2606:4700:4700::1112";
+          tls_auth_name = "cloudflare-dns.com";
+        }
+        {
+          address_data = "2606:4700:4700::1002";
+          tls_auth_name = "cloudflare-dns.com";
+        }
+        {
+          address_data = "9.9.9.9";
+          tls_auth_name = "dns.quad9.net";
+        }
+        {
+          address_data = "149.112.112.112";
+          tls_auth_name = "dns.quad9.net";
+        }
+        {
+          address_data = "2620:fe::fe";
+          tls_auth_name = "dns.quad9.net";
+        }
+        {
+          address_data = "2620:fe::9";
+          tls_auth_name = "dns.quad9.net";
+        }
+      ];
+    };
+  };
+
+  networking.nameservers = [ "::1" "127.0.0.1" ];
+  services.resolved = {
+    enable = true;
+    fallbackDns = [ "2606:4700:4700::1112" "2606:4700:4700::1002" "1.1.1.2" "1.0.0.2" ];
   };
 
   system.stateVersion = "24.05";
--- justfile
+++ justfile
@@ -2,4 +2,4 @@ deploy:
     nix run github:nix-community/nixos-anywhere -- --flake .#hetzner-cloud nix-onion-proxy
 
 rebuild:
-    nixos-rebuild switch --flake .#hetzner-cloud --target-host nix-onion-proxy
+    nix run nixpkgs#nixos-rebuild -- switch --flake .#hetzner-cloud --target-host nix-onion-proxy
