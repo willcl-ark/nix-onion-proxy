c27d84a15620a6cc51cc8330cc176ccb9547da30
willcl-ark <will@256k1.dev>
2024-11-27 11:09:49 +0000

    use unbound instead of stubby
    This seems to be faster/more reliable

--- configuration.nix
+++ configuration.nix
@@ -56,52 +56,30 @@
   };
 
   ## DNS-over-TLS
-  services.stubby = {
+  services.unbound = {
     enable = true;
     settings = {
-      # ::1 cause error, use 0::1 instead
-      listen_addresses = [ "127.0.0.1" "0::1" ];
-      # https://github.com/getdnsapi/stubby/blob/develop/stubby.yml.example
-      resolution_type = "GETDNS_RESOLUTION_STUB";
-      dns_transport_list = [ "GETDNS_TRANSPORT_TLS" ];
-      tls_authentication = "GETDNS_AUTHENTICATION_REQUIRED";
-      tls_query_padding_blocksize = 128;
-      idle_timeout = 10000;
-      round_robin_upstreams = 1;
-      tls_min_version = "GETDNS_TLS1_3";
-      dnssec = "GETDNS_EXTENSION_TRUE";
-      upstream_recursive_servers = [
+      server = {
+        interface = ["127.0.0.1" "::1"];
+        access-control = [
+          "127.0.0.0/8 allow"
+          "::1/128 allow"
+        ];
+        # Use TCP for upstream queries (better for TLS)
+        tcp-upstream = true;
+        # Enable DNSSEC validation
+        auto-trust-anchor-file = "/var/lib/unbound/root.key";
+      };
+      forward-zone = [
         {
-          address_data = "1.1.1.2";
-          tls_auth_name = "cloudflare-dns.com";
-        }
-        {
-          address_data = "1.0.0.2";
-          tls_auth_name = "cloudflare-dns.com";
-        }
-        {
-          address_data = "2606:4700:4700::1112";
-          tls_auth_name = "cloudflare-dns.com";
-        }
-        {
-          address_data = "2606:4700:4700::1002";
-          tls_auth_name = "cloudflare-dns.com";
-        }
-        {
-          address_data = "9.9.9.9";
-          tls_auth_name = "dns.quad9.net";
-        }
-        {
-          address_data = "149.112.112.112";
-          tls_auth_name = "dns.quad9.net";
-        }
-        {
-          address_data = "2620:fe::fe";
-          tls_auth_name = "dns.quad9.net";
-        }
-        {
-          address_data = "2620:fe::9";
-          tls_auth_name = "dns.quad9.net";
+          name = ".";
+          forward-tls-upstream = "yes";
+          forward-addr = [
+            "1.1.1.2@853#security.cloudflare-dns.com"
+            "1.0.0.2@853#security.cloudflare-dns.com"
+            "2606:4700:4700::1112@853#security.cloudflare-dns.com"
+            "2606:4700:4700::1002@853#security.cloudflare-dns.com"
+          ];
         }
       ];
     };
